a660d8c63d9cf392fdabd90f4aadee1970bf7ae9
Guilherme Passos <guipassos7@gmail.com>
2022-01-23 11:58:19 -0300

    ajuste url prd

--- src/app/util/ServiceBase.ts
+++ src/app/util/ServiceBase.ts
@@ -1,13 +1,11 @@
+import { env } from "process";
 import { environment } from "../../environments/environment";
 
 export class ServiceBase {
-    
+
     baseURL: string = '';
 
     constructor() {
-        if (environment.production)
-            this.baseURL = environment.apiUrlProd;
-        else
-            this.baseURL = environment.apiUrl;
+        this.baseURL = environment.apiUrl;
     }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
   apiUrl: 'http://odontoyou-ponto-back.herokuapp.com',
-  apiUrlProd: 'http://odontoyou-ponto-back.herokuapp.com'
 };
